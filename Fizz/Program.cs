56815453cbee15d651199b0822552cb27ea1b67c
Devin Riley <devin.riley144@gmail.com>
2023-01-25 13:43:06 -0600

    Add project files.

---
+++ Fizz/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace Fizz
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for(var i = 0; i <= 100; i++)
+            {
+                Console.WriteLine($"{i}: {FizzBuzz(i)}");
+            }
+        }
+
+            public static string FizzBuzz(int num)
+            {
+                var answer = "";
+
+
+                if (num % 3 == 0)
+                {
+                    answer += "Fizz";
+                }
+                if (num % 5 == 0)
+                {
+                    answer += "Buzz";
+                }
+
+                return answer;
+
+
+            }    
+
+         
+    }
+}
